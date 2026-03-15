fb97e51bd0860d7191cccc3744d34c86668c4c05
Rolf Kristensen <sweaty1@hotmail.com>
2026-03-15 13:09:38 +0100

    HttpClientTarget - HttpClient with support for batching

--- src/NLog.Targets.Network/Targets/NetworkTarget.cs
+++ src/NLog.Targets.Network/Targets/NetworkTarget.cs
@@ -35,6 +35,7 @@ namespace NLog.Targets
 {
     using System;
     using System.Collections.Generic;
+    using System.Security.Cryptography.X509Certificates;
     using System.Text;
     using System.Threading;
     using NLog.Common;
